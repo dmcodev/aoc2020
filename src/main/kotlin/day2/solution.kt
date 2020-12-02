852e4b27765b74d37e3b009ef87e00f7dec5196f
Damian Malczyk <damian.malczyk.dev@gmail.com>
2020-12-02 09:46:50 +0100

    Day 2

---
+++ src/main/kotlin/day2/solution.kt
@@ -0,0 +1,25 @@
+package day2
+
+import util.loadInput
+
+private val pattern = Regex("^(\\d+)-(\\d+)\\s+(\\w):\\s+(.+)$")
+
+fun main() {
+    val input = loadInput(2)
+    println(
+        input.count {
+            val match = requireNotNull(pattern.find(it))
+            val (_, min, max, char, password) = match.groupValues
+            password.count { it == char[0] }
+                .takeIf { it >= min.toInt() && it <= max.toInt() } != null
+        }
+    )
+    println(
+        input.count {
+            val match = requireNotNull(pattern.find(it))
+            val (_, first, second, char, password) = match.groupValues
+            sequenceOf(password[first.toInt() - 1], password[second.toInt() - 1])
+                .count { it == char[0] } == 1
+        }
+    )
+}
