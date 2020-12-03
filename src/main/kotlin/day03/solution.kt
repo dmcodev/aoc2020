630c89d2c1c3546d37a2b93dc475d7532bd9ee96
Damian Malczyk <damian.malczyk.dev@gmail.com>
2020-12-03 23:44:29 +0100

    Day 3

---
+++ src/main/kotlin/day03/solution.kt
@@ -0,0 +1,27 @@
+package day03
+
+import util.loadInput
+
+fun String.isTree(position: Int): Boolean =
+    get(position - (length * (position / length))) == '#'
+
+fun List<String>.countTrees( deltaX: Int, deltaY: Int): Int {
+    var x = 0
+    var y = 0
+    var count = 0
+    while (y < size) {
+        if (get(y).isTree(x)) count++
+        y += deltaY
+        x += deltaX
+    }
+    return count
+}
+
+fun main() {
+    val input = loadInput(3)
+    val trees = sequenceOf(1 to 1, 3 to 1, 5 to 1, 7 to 1, 1 to 2)
+        .map { input.countTrees(it.first, it.second) }
+        .toList()
+    println(trees[1])
+    println(trees.reduce(Int::times))
+}
--- src/main/kotlin/util/input.kt
+++ src/main/kotlin/util/input.kt
@@ -3,7 +3,7 @@ package util
 import java.nio.charset.StandardCharsets
 
 fun loadInput(day: Int, fileName: String = "input"): List<String> =
-    object {}.javaClass.getResourceAsStream("/day$day/$fileName")
+    object {}.javaClass.getResourceAsStream("/day${day.toString().padStart(2, '0')}/$fileName")
         .use { it.bufferedReader(StandardCharsets.UTF_8).lineSequence().toList() }
 
 fun loadInputLongs(day: Int, fileName: String = "input"): List<Long> =
